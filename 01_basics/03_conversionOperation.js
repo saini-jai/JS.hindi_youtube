a437089cd4fa183e418eb62b6826ee05ef781c35
Jai Saini <141661933+saini-jai@users.noreply.github.com>
2024-03-14 19:51:24 +0000

    converstion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "99mnh"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "52" => 52
+// "33abc" => NaN type number
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "rupesh" => true
