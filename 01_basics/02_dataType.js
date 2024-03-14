442867e402bfb3b17897eadcea3cfc3c932719c0
Jai Saini <141661933+saini-jai@users.noreply.github.com>
2024-03-14 19:34:12 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3 + 3)   we are using nodejs, not browser
+
+console.log( 3 + 3 )
+// code readabilitty should be high
+console.log("jai")
+
+let name = "Jai"
+let age = 20
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => 
+
+
+// object 
+
+
+console.log(typeof undefined); // underfined
+console.log(typeof null);   // object
